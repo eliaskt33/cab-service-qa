c1e13c98b9a7c93fb2f87ec17bd54f8ce08fa035
Elias <egty3328@gmail.com>
2026-01-21 16:19:19 +0200

    Add TestNG automation framework with pom.xml and testng.xml

---
+++ src/test/java/com/cabservice/tests/SmokeTest.java
@@ -0,0 +1,13 @@
+﻿package com.cabservice.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SmokeTest {
+
+    @Test
+    public void basicSmokeTest() {
+        String serviceName = "Cab Service";
+        Assert.assertTrue(serviceName.contains("Cab"), "Service name validation failed!");
+    }
+}
